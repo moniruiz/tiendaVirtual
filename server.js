a4df34e81991ff3df6fa1405dc65735944fb7c38
moniruiz <rumoni85@gmail.com>
2021-07-02 22:16:36 -0300

    implementacion de handlebars

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require("express");
+const exhbs = require("express-handlebars");
+const path = require("path");
 const app = express();
 
-app.get("/", function(req, res) {
-    res.send("HOLA MUNDO");
-})
-app.listen(3000 , function(){
+app.engine("handlebars", exhbs({
+    defaultLayout: "main",
+    layoutsDir: "views/layouts"
+}));
+
+app.set("view engine", "handlebars");
+
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", function (req, res) {
+    res.render("products",{
+        nombre: "Monica"
+    });
+});
+
+app.listen(3000, function () {
     console.log("iniciando server en puerto... 3000");
-})
+});
